321d42e746c72d7ac8953edafb05196e3dac6a04
Kate Thompson <katyjmt@gmail.com>
2023-09-14 11:37:10 +1000

    Sign up, Login, Logout user routes created

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// User signs up => CREATE new user:
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// User logs in => validate + update loggedIn status
+router.post('/login', async (req, res) => {
+  try {
+    // Find user in db with email that matches the one submitted
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    // If email can't be found, return error message
+    if (!dbUserData) {
+      res.status(400).json({ message: 'Incorrect email or password, please try again. '});
+      return;
+    }
+
+    // Use checkPassword (bcrypt - defined in User model) to compare pw with hashed one in db
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    // If password isn't valid, return error message
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect email or password, please try again.'});
+      return;
+    }
+
+    // If login success, create sessions boolean variable 'loggedIn' and set to true
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      res.status(200).json({ user: dbUserData, message: 'You\'re now logged in' });
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// User logs out => update loggedIn status
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      // 204 - server successfuly processed request, no content returned
+      res.status(204).end();
+    });
+  } else {
+    // 404 - session with loggedIn status could not be found
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
--- models/users.js
+++ models/users.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
